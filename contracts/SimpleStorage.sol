b01789e7554a131b29308af2dc2dbf3cb26ead4f
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2024-01-23 16:14:34 +0100

    HELLO

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -1,2 +1,18 @@
 // SPDX-License-Identifier: MIT
-pragma solidity  >=0.8.7 <0.9.0;
+pragma solidity >=0.8.8;
+
+contract SimpleStorage {
+    // bool hasFavoriteNumber = false;
+    // string favoriteNumberInText = "Five";
+    // int favoriteInt = -5;
+    // address myAddress = 0x6DaeA8245E381ac1B2B6C3AF8A2A8e7c58eB1CA4;
+    // bytes32 favoriteBytes = "cat";
+    uint public favoriteNumber;
+
+    function store (uint256 _favoriteNumber) public {
+        favoriteNumber = _favoriteNumber;
+        favoriteNumber = favoriteNumber + 1;
+    }
+} 
+
+//0x7AF458610332Ae84d25B600ef0b5a432768f322A
