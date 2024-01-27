2ba773b3ddf79096126fe74b0444c5972ab51bb6
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2024-01-27 14:46:39 +0100

    hello

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -11,12 +11,17 @@ contract SimpleStorage {
 
     function store (uint256 _favoriteNumber) public {
         favoriteNumber = _favoriteNumber;
-        uint256 tesVar = 5;
+        retrieve();
     }
 
-    function something() public {
-        tesVar = 6;
+    //view, pure
+    function retrieve() public view returns (uint256){
+        return favoriteNumber;
     }
+
+    // function add() public  pure returns (uint256) {
+    //     return (1 + 1);
+    // }
 } 
 
 //0x7AF458610332Ae84d25B600ef0b5a432768f322A
