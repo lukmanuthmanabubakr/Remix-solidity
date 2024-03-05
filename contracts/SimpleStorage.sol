b811e8556dd162e91d5a422fc22490f693957510
dev_dimeji✔ <123726979+lukmanuthmanabubakr@users.noreply.github.com>
2024-03-05 18:33:17 +0100

    Update SimpleStorage.sol

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -1,8 +1,7 @@
 
 pragma solidity >=0.8.8;
 
-contract SimpleStorage {
-    // bool hasFavoriteNumber = false;
+contract SimpleStorage {
     // string favoriteNumberInText = "Five";
     // int favoriteInt = -5;
     // address myAddress = 0x6DaeA8245E381ac1B2B6C3AF8A2A8e7c58eB1CA4;
